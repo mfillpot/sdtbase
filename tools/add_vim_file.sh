fa1be554917b7bd21088c25e761552fd861a20f0
Matthew Fillpot <mfilpot@gmail.com>
2012-12-28 17:55:27 -0500

    updated add_vim_file.sh to work around new directory structure

--- tools/add_vim_file.sh
+++ tools/add_vim_file.sh
@@ -4,15 +4,16 @@ VIMRC=~/.vimrc
 EXTENSION="slackdoc"
 FILETYPE="slackdocs"
 SYNTAXFILE="slackdocs.vim"
+SYNTAX_DIR="syntax_files"
+TOOLS_DIR="tools"
 
-# Define the path to the script since the same directory
-#  contains the syntax file
-ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd) 
+# Define the path to the repo directory
+REPO_PATH=$(dirname $(cd `dirname "${BASH_SOURCE[0]}"` && pwd) )
 
 # removed existing slackdoc syntax template lines if they exist
 sed -i /$FILETYPE/d $VIMRC
 
 # Write the new lines to the vimrc file
-echo "\" Add a pointer to the $FILETYPE vim syntax file">>$VIMRC
-echo "au BufRead,BufNewFile *.$EXTENSION set filetype=$FILETYPE">>$VIMRC
-echo "au! Syntax $FILETYPE source $ABSOLUTE_PATH/$SYNTAXFILE">>$VIMRC
+echo "\" Add a pointer to the $FILETYPE vim syntax file">$VIMRC
+echo "au BufRead,BufNewFile *.$EXTENSION set filetype=$FILETYPE">$VIMRC
+echo "au! Syntax $FILETYPE source $REPO_PATH/$SYNTAX_DIR/$SYNTAXFILE">$VIMRC
