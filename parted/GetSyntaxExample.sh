9aed7fc507cc742f17c8d88c2f699cae87cd41df
Marcin Herda <mherda@slackword.net>
2013-01-26 17:07:13 +0000

    Removed some unnumbered files

--- parted/GetSyntaxExample.sh
+++
@@ -1,5 +0,0 @@
-GetSyntaxExample () {
-      PullPage "wiki:syntax" ${SD_DIR}
-      PrintText 0 "${NRMTXT}" "The example syntax file is now located at
-${SD_DIR}/wiki-syntax.${EXTENSION}" 1
-}
--- parted/SetTemps.sh
+++
@@ -1,8 +0,0 @@
-SetTemps() {
-  # Sets an array with the contents of the template index 
-  if [ ! -f ${TEMP_FILE} ]; then
-    RefreshTemplates
-  else
-    TEMPS=( $(cat ${TEMP_FILE}) )
-  fi
-}
