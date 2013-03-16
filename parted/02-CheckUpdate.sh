8157b8ab3c1e12ad069824a6434e6ca735699834
Matthew Fillpot <mfilpot@gmail.com>
2013-03-16 08:24:17 -0400

    Added comments and moved lookup command to prompt before fetch operation

--- parted/02-CheckUpdate.sh
+++ parted/02-CheckUpdate.sh
@@ -1,9 +1,16 @@
 CheckUpdate() {
 # Checks githhub for a new version of the file and downloads it
 
-  NEW_VERSION=$(lynx -dump ${VERSION_URL} | grep slackdoctool | cut -d "=" -f 2)
+  # Display a message indicating that the script is checking for a new
+  # version
   printf "\n${BLDTXT}%s${NRMTXT}\n" \
     "Checking for an updated script."
+
+  # get the new version number from the repo
+  NEW_VERSION=$(lynx -dump ${VERSION_URL} | grep slackdoctool | cut -d "=" -f 2)
+
+  # Test to see if an update is available, display a message indicatin the
+  # status
   if [ "${NEW_VERSION}" = "${VERSION}" ]; then
     printf "\n${SCSTXT}%s${NRMTXT}\n" \
       "Your copy is up to date"
